0e534a0fbccdc5ca406d3122f4e05a1cb340938c
Syed Musa <musabinsyed@gmail.com>
2023-10-28 20:56:49 +0600

    finished an server side

--- _jwt_token_access.js
+++ _jwt_token_access.js
@@ -17,6 +17,17 @@
  * 1. set cookies with HTTP only for development
  * secure: false
  * 2. cors
- * 3. clinet side axios setting
- * 
+ * 3. app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+  }));
+ * 4. clinet side axios setting
+ * 5. in axios set withCredentials: true
+ */
+
+/**
+ * 1. To send cookies from the client make sure
+ * you added withCredential: true
+ * for the api call using axios
+ * 2. 
  */
