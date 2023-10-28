1a2ef2b3d66f3a19e71f48cb26c264774e988f3e
Syed Musa <musabinsyed@gmail.com>
2023-10-28 09:36:35 +0600

    added a all setup section

---
+++ _jwt_token_access.js
@@ -0,0 +1,22 @@
+/**
+ * 1. Install jsonwebtoken
+ * 2. jwt.sign (payload, secret, {expiresin:})
+ * 3. Token client
+ * 
+ */
+
+/**
+ * How to store token in the client side
+ * 1. Money --> ok type
+ * 2. local storage --> ok type(XSS)
+ * 3. cookies: http only
+ * 
+ */
+
+/**
+ * 1. set cookies with HTTP only for development
+ * secure: false
+ * 2. cors
+ * 3. clinet side axios setting
+ * 
+ */
